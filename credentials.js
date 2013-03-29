cbeb7b3ec6601f57f58f52aabc2b824baa98abd9
Aaron Randall <aaronjrandall@gmail.com>
2013-03-29 17:12:37 +0000

    Update credentials.js to access Heroku environment vars.

--- credentials.js
+++ credentials.js
@@ -1,6 +1,6 @@
 var twit = new twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token_key: process.env.access_token_key,
+  access_token_secret: process.env.access_token_secret
 });
