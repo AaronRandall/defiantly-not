d3dfb54f8c437a7d3f0df9836b11aecf35606126
Aaron Randall <aaronjrandall@gmail.com>
2013-03-30 17:49:10 +0000

    Remove socket.io server from web process.

--- web.js
+++ web.js
@@ -20,7 +20,7 @@ app.get('/', function (req, res) {
   
   script_url = 'http://localhost';
   if(process.env.PORT) {
-    script_url = 'http://defiantly-not.herokuapp.com';
+    script_url = 'http://defiantly-not-daemon.herokuapp.com';
   }
   
   res.render(__dirname + '/public/index.html', {
@@ -41,35 +41,3 @@ app.get('/tweets.js', function (req, res) {
 app.get('/timeago.js', function (req, res) {
   res.sendfile(__dirname + '/public/timeago.js');
 });
-
-
-var io = require('socket.io').listen(app);
-// Heroku won't actually allow us to use WebSockets
-// so we have to setup polling instead.
-// https://devcenter.heroku.com/articles/using-socket-io-with-node-js-on-heroku
-io.configure(function () {
-  io.set("transports", ["xhr-polling"]);
-  io.set("polling duration", 10);
-});
-//io.set('transports', ['xhr-polling']); io.set('polling duration', 10);
-var query = "defiantly";
-io.sockets.on('connection', function (socket) { 
-  console.log('STARTING SOCKET CONNECTION');
-  twit.stream('user', {track: query}, function(stream) {
-    stream.on('data', function (data) {
-      if(data.text) {
-        if(!data.text.startsWith("RT")) {
-          data.split = data.text.split(" ")
-          socket.volatile.emit('tweet', data);
-        }
-      }
-    });
-  });
-  socket.on('disconnect', function () {
-    console.log('SOCKET DISCONNECTED');
-  });
-});
-
-
-var fs = require('fs');
-eval(fs.readFileSync('credentials.js')+'');
